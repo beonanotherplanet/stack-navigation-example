f913944e0ff7021e1a6f3f99ad42a72a72e3b899
div-seungha <sh.kim.x928@gmail.com>
2025-09-21 22:07:22 +0900

    Fix eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,7 @@ export default tseslint.config([
         files: ["src/stackflow/**"],
         rules: {
           "@typescript-eslint/no-explicit-any": "off",
+          "@typescript-eslint/no-unused-vars": "warn",
         },
       },
     ],
